96aba57d8331b672b8edb698f9fbb7bedec3d05f
saganshul <sganshuliiith@gmail.com>
2015-10-29 23:27:54 +0530

    Answer field made integer

--- buzz2k15/gordian_knot/models.py
+++ buzz2k15/gordian_knot/models.py
@@ -7,7 +7,7 @@ class Question(models.Model):
 	level_list = [(1,1), (2,2), (3,3), (4,4), (5,5), (6,6), (7,7), (8,8), (9,9), (10,10), (11,11), (12,12), (13,13), (14,14), (15,15), (16,16), (17,17), (18,18), (19,19), (20,20) ]
 	level = models.IntegerField (choices = level_list, default = 0)
 	question_text = models.TextField ('Question', max_length = 1000)
-	correct_answer = models.CharField(max_length = 100)
+	correct_answer = models.IntegerField(blank=False)
 	def __unicode__(self):
 		return unicode(self.level)
 
